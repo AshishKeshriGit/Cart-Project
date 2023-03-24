4cd0de25ba00a7a371b0d43d8bb29664cce26f81
Ashish <ashishkeshri3016@gmail.com>
2023-03-24 06:25:40 +0530

    Firebase DB is added

--- src/index.js
+++ src/index.js
@@ -3,6 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+// import * as firebase from 'firebase';
+// import 'firebase/firestore';
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBjqn1VYuJEkmUO7D_0CUMR0K5hWOogMoo",
+  authDomain: "cart-8c369.firebaseapp.com",
+  projectId: "cart-8c369",
+  storageBucket: "cart-8c369.appspot.com",
+  messagingSenderId: "221593119891",
+  appId: "1:221593119891:web:199bcfa454e11c91fb7166"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
